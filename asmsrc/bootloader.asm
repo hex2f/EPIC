04d6cc625f0b049a5fa1e9706f6b935d1eeb5215
RekkyRek <haslun16@edu.vilhelmina.se>
2018-09-02 15:46:12 +0200

    Added disk and c++

--- asmsrc/bootloader.asm
+++ asmsrc/bootloader.asm
@@ -4,13 +4,31 @@ org 0x7c00
 boot:
 	mov ax, 0x2401
 	int 0x15
+
 	mov ax, 0x3
 	int 0x10
+
+	mov [disk],dl
+
+	mov ah, 0x2    ;read sectors
+	mov al, 1      ;sectors to read
+	mov ch, 0      ;cylinder idx
+	mov dh, 0      ;head idx
+	mov cl, 2      ;sector idx
+	mov dl, [disk] ;disk idx
+	mov bx, copy_target;target pointer
+	int 0x13
 	cli
 	lgdt [gdt_pointer]
 	mov eax, cr0
 	or eax,0x1
 	mov cr0, eax
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
 	jmp CODE_SEG:boot2
 gdt_start:
 	dq 0x0
@@ -32,32 +50,29 @@ gdt_end:
 gdt_pointer:
 	dw gdt_end - gdt_start
 	dd gdt_start
-
+disk:
+	db 0x0
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
+times 510 - ($-$$) db 0
+dw 0xaa55
+copy_target:
 bits 32
+	hello: db "Hello more than 512 bytes world!!",0
 boot2:
-	mov ax, DATA_SEG
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
 	mov esi,hello
 	mov ebx,0xb8000
 .loop:
 	lodsb
 	or al,al
 	jz halt
-	or eax,0x0100
+	or eax,0x0F00
 	mov word [ebx], ax
 	add ebx,2
 	jmp .loop
 halt:
 	cli
 	hlt
-hello: db "Hello world!",0
 
-times 510 - ($-$$) db 0
-dw 0xaa55
+times 1024 - ($-$$) db 0
